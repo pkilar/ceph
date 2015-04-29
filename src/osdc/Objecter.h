6128e89a48fc069420705690a5d16cac6c0184a3
John Spray <john.spray@redhat.com>
2015-04-29 11:35:03 +0100

    librados: add get_progress to object iterators
    Signed-off-by: John Spray <john.spray@redhat.com>

--- src/osdc/Objecter.h
+++ src/osdc/Objecter.h
@@ -1322,6 +1322,11 @@ public:
     // ...total m workers.  Or m=0 if not using multiple workers.
     uint32_t worker_m;
 
+    // Lowest PG we will touch (inclusive)
+    uint32_t pg_min;
+    // Highest PG we will touch (inclusive)
+    uint32_t pg_max;
+
     uint64_t object_hash_low;
     // Drop objects greater than hash_high (not equal to)
     // In HashIndex nibble encoding
@@ -1372,6 +1377,8 @@ public:
     bool is_sharded() const {
       return worker_m > 0;
     }
+
+    float get_progress() const;
   };
 
   struct C_NList : public Context {
@@ -2464,6 +2471,12 @@ public:
 
   void list_nobjects(NListContext *p, Context *onfinish);
   uint32_t list_nobjects_seek(NListContext *p, uint32_t pos);
+  /**
+   * Approximate position in assigned object hash range,
+   * as a real number 0.0<=n<=1.0
+   */
+  float list_nobjects_progress(NListContext *list_context) const;
+
   void list_objects(ListContext *p, Context *onfinish);
   uint32_t list_objects_seek(ListContext *p, uint32_t pos);
 
