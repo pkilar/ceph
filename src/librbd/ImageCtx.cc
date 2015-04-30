f17257f15fc956d365bf77a9f3e10b971bdf2877
Jason Dillaman <dillaman@redhat.com>
2015-04-30 15:34:43 -0400

    librbd: execute flush completion outside of cache_lock
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/librbd/ImageCtx.cc
+++ src/librbd/ImageCtx.cc
@@ -706,7 +706,8 @@ namespace librbd {
                  << unclean << " bytes remain" << dendl;
       r = -EBUSY;
     }
-    on_finish->complete(r);
+
+    aio_work_queue.queue(on_finish, 0);
   }
 
   void ImageCtx::clear_nonexistence_cache() {
